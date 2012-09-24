fa96b7d7628ae938ac1245996a3f9b13cd6dad8a
S t u a r t <github@sequation.com.au>
2012-09-24 12:10:49 +1000

    Added cascading delete for associated tickets when a project is deleted.

--- spec/integration/deleting_projects_spec.rb
+++ spec/integration/deleting_projects_spec.rb
@@ -2,8 +2,11 @@ require "spec_helper"
 
 feature 'Deleting projects' do
 
+  before do
+    @textmate_2 = Factory(:project, :name => 'TextMate 2')
+  end
+
   scenario "Deleting a project" do
-    Factory(:project, :name => 'TextMate 2')
     visit '/'
     click_link 'TextMate 2'
     click_link 'Delete Project'
@@ -11,5 +14,17 @@ feature 'Deleting projects' do
 
     visit '/'
     page.should_not have_content('TextMate 2')
+  end
+
+  scenario 'Deleting a project with tickets also deletes the tickets' do
+    ticket = Factory(:ticket,
+                     :project => @textmate_2,
+                     :title => 'Make it shiny!',
+                     :description => 'Gradients! Starbursts! Oh my!')
+    visit '/'
+    click_link 'TextMate 2'
+    click_link 'Delete Project'
+
+    expect{ Ticket.find(ticket) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
